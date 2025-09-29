e35ca52994473b1078e22de8457624c25b0ad302
panupongartpairin <156408367+panupongartpairin@users.noreply.github.com>
2025-09-29 12:05:13 +0700

    Update Plants vs Brainrots EN.lua

--- Plants vs Brainrots EN.lua
+++ Plants vs Brainrots EN.lua
@@ -1,2 +1,2 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/panupongartpairin/UFO-HUB-X-UI/refs/heads/main/UFO%20HUB%20X%20UI.lua"))()
 local Window = Library.CreateLib("TITLE", "UFOHUBX")
